a300e3dd2f269b122d1f22d2e3202c0fa3208a26
abhi96869229 <70836055+abhi96869229@users.noreply.github.com>
2021-06-06 17:43:55 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Change all Values Here Before you use.....
+
+bot_token = "1672461736:AAFw1LOuP-G46f_VaS-87PAhGZq0rVqSj0c"
+sudo_chat_id = -1001373143342
+
+ARQ_API_KEY = "JTTZQL-WQLKXF-QPTTBC-KRGLBU-ARQ" # Get it from @ARQRobot. Needed for Saavn and Deezer.
+DeezerQuality = 9 # 1 = 128k Audio Quality
+                  # 3 = 320k Audio Quality
+                  # 9 = FLAC Format Very High Quality !
+
+# Customization Changes
+Photo_Theme = True    # True - Album Art with Now playing Display
+                      # False - No Album Art with Now Playing Display
